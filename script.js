5bddbf9f43393b279d7442bce773b19ffc339572
L&J Brothers <41821541+Liubomyr17@users.noreply.github.com>
2019-07-29 10:53:11 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+// Blue Print
+// Factory Functions and Constructor Functions
+// Constructor Functions
+
+function Person(name, lastName) {
+    this.name = name;
+    this.lastName = lastName;
+    this.fullName = function () {
+        console.log(
+            `Hello my full name is ${this.name} ${this.lastName} and I like Angular`
+        );
+    }
+    // console.log(this);
+}
+
+const john = new Person('john', 'anderson');
+const bob = new Person('bob', 'jordan');
+const susy = new Person('susy', 'cooper');
+
+john.fullName();
+bob.fullName();
+susy.fullName();
+
+
+function createPerson(name, lastName) {
+    return {
+        name: name,
+        lastName: lastName,
+        fullName: function () {
+            console.log(
+                `Hello my full name is ${this.name} ${this.lastName} and I like React`
+            );
+        }
+    };
+}
